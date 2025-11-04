256d7bc7b5d14575e4e579867d74d558216cad0c
Your Name <your.email@example.com>
2025-11-04 13:35:12 +0200

    Add Customer model with Lombok annotations and fields

--- src/main/java/com/uber_like/ride_sharing/server/model/Customer.java
+++ src/main/java/com/uber_like/ride_sharing/server/model/Customer.java
@@ -1,4 +1,12 @@
-package com.uber_like.ride_sharing.server.model;
+package com.uber_like.ride_sharing.model;
 
+import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class Customer {
+    private String username;
+    private String email;
+    private String password;
 }
