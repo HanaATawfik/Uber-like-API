410a1e593f5dfe6b8b54cf49d571788d60a91bcd
Your Name <your.email@example.com>
2025-11-04 13:35:24 +0200

    Add Driver model with Lombok annotations and fields

--- src/main/java/com/uber_like/ride_sharing/server/model/Driver.java
+++ src/main/java/com/uber_like/ride_sharing/server/model/Driver.java
@@ -1,4 +1,11 @@
-package com.uber_like.ride_sharing.server.model;
+package com.uber_like.ride_sharing.model;
 
+import lombok.Data;
+
+@Data
 public class Driver {
+    private String username;
+    private String email;
+    private String password;
+    private String status = "available";
 }
